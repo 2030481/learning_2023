b7e6eac32fac26d1491622baecb7977f1e83c47c
Ravi Ranjan Kumar <92367233+2030481@users.noreply.github.com>
2023-06-15 11:26:15 +0000

    commit

--- Module1/Day1/Question5.c
+++ Module1/Day1/Question5.c
@@ -4,4 +4,39 @@
     - Toggle 16th bit, if operation type is 3
    You can consider the below function prototype,
     - bit_operations(int num, int oper_type);*/
-    
+    #include <stdio.h>
+
+int bit_operations(int num, int oper_type) {
+    switch (oper_type) {
+        case 1:  // Set 1st bit
+            num |= (1 << 0);
+            break;
+        case 2:  // Clear 31st bit
+            num &= ~(1 << 31);
+            break;
+        case 3:  // Toggle 16th bit
+            num ^= (1 << 15);
+            break;
+        default:
+            printf("Invalid operation type\n");
+            break;
+    }
+
+    return num;
+}
+
+int main() {
+    int num, oper_type;
+
+    printf("Enter an integer (32 bits): ");
+    scanf("%d", &num);
+
+    printf("Enter operation type (1, 2, or 3): ");
+    scanf("%d", &oper_type);
+
+    int result = bit_operations(num, oper_type);
+
+    printf("Result: %d\n", result);
+
+    return 0;
+}
