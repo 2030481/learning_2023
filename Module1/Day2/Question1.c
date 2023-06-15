853b6eee601744352bbffaa863e0bdecac2d204d
Ravi Ranjan Kumar <92367233+2030481@users.noreply.github.com>
2023-06-15 11:37:48 +0000

    commit

--- Module1/Day2/Question1.c
+++ Module1/Day2/Question1.c
@@ -7,3 +7,21 @@ https://baseconvert.com/ieee-754-floating-point
 Topics to be covered
 - Pointers
 - Bitwise Operators*/
+#include <stdio.h>
+
+int main() {
+    double x = 0.7;
+    unsigned long long* ptr = (unsigned long long*)&x;
+    unsigned long long exponent = (*ptr & 0x7FF0000000000000) >> 52;
+    
+    printf("Exponent in hex: 0x%llX\n", exponent);
+    printf("Exponent in binary: 0b");
+    
+    for (int i = 51; i >= 0; i--) {
+        unsigned long long bit = (exponent >> i) & 1;
+        printf("%llu", bit);
+    }
+    
+    printf("\n");
+    return 0;
+}
