22b3a1dbb93c1edf3b3334601be24c82fcfc3dae
2030481 <rvravi163@gmail.com>
2023-06-19 17:20:24 +0000

    Day 4 : String Q Commit

---
+++ Module1/Day4/ConvertStringAsInteger_String.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char str[] = "12345"; // Example string
+
+    int num = atoi(str);
+
+    printf("String: %s\n", str);
+    printf("Integer: %d\n", num);
+
+    return 0;
+}
