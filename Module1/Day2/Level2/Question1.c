bc9a081f6150892db83b9d411a5309cd4128b933
2030481 <rvravi163@gmail.com>
2023-06-19 17:33:05 +0000

    Commit New QUESTION

---
+++ Module1/Day2/Level2/Question1.c
@@ -0,0 +1,10 @@
+/*
+Write a program to swap every alternate element of a given array starting for element 0
+For example, let's say we have a[5] = {10, 20, 30, 40, 50}, the result should be a[5] = {30, 20, 10, 40, 50}
+one more example a[5] = {10, 20, 30, 40, 50, 60}, the result should be a[5] = {30, 20, 10, 60, 50, 40}
+
+Topics to be covered
+- Arrays
+- Loops
+- Basic Operators
+*/
