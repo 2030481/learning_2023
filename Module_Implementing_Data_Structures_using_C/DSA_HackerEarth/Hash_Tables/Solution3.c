fe3dc0adfaa7733dc9367b8fd9ddc0a437055d1a
2030481 <rvravi163@gmail.com>
2023-07-31 17:28:51 +0000

    Commit:DSA:HackerEarth

--- Module_Implementing_Data_Structures_using_C/DSA_HackerEarth/Hash_Tables/Solution3.c
+++ Module_Implementing_Data_Structures_using_C/DSA_HackerEarth/Hash_Tables/Solution3.c
@@ -2,11 +2,16 @@
 Problem HashTables
 Little Jhools And Magical Jewels
 Problem
-Little Jhool is still out of his mind - exploring all his happy childhood memories. And one of his favorite memory is when he found a magical ghost, who promised to fulfill one of Little Jhool's wish.
+Little Jhool is still out of his mind - exploring all his happy childhood memories.
+ And one of his favorite memory is when he found a magical ghost, who promised to fulfill one of Little Jhool's wish.
 
-Now, Little Jhool was a kid back then, and so he failed to understand what all could he have asked for from the ghost. So, he ends up asking him something very simple. (He had the intuition that he'd grow up to be a great Mathematician, and a Ruby programmer, alas!) He asked the ghost the power to join a set of *the letters r, u, b and y * into a real ruby. And the ghost, though surprised, granted Little Jhool his wish...
+Now, Little Jhool was a kid back then, and so he failed to understand what all could he have asked for from the ghost.
+ So, he ends up asking him something very simple. (He had the intuition that he'd grow up to be a great Mathematician,
+  and a Ruby programmer, alas!) He asked the ghost the power to join a set of *the letters r, u, b and y * into a real ruby.
+   And the ghost, though surprised, granted Little Jhool his wish...
 
-Though he regrets asking for such a lame wish now, but he can still generate a lot of real jewels when he's given a string. You just need to tell him, given a string, how many rubies can he generate from it?
+Though he regrets asking for such a lame wish now, but he can still generate a lot of real jewels when he's given a string.
+ You just need to tell him, given a string, how many rubies can he generate from it?
 
 Input Format:
 The first line contains a number t - denoting the number of test cases.
